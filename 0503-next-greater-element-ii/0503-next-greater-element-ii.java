b19abf2631952357585f8c0f10bbb71b1c3e0e77
Ritendra kumar <ritendrabhediya@gmail.com>
2026-09-01 20:50:36 +0530

    Time: 17 ms (23.12%), Space: 48.3 MB (75.99%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -5,20 +5,23 @@ class Solution {
         int[] ans = new int[n];
         Arrays.fill(ans, -1);
 
-      for(int i=0;i<nums.length;i++)
-      {
-        int currel=nums[i];
-        for(int j=1;j<nums.length;j++)
-        {
-           int ind = (i + j) % n;
-            if(nums[ind]>currel)
-            {
-                ans[i]=nums[ind];
-                break;
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+
+            int index = i % n;
+
+            while (!st.isEmpty() && nums[index] >= nums[st.peek()]) {
+                st.pop();
             }
+
+            if (!st.isEmpty()) {
+                ans[index] = nums[st.peek()];
+            }
+
+            st.push(index);
         }
-       
-      }
-       return ans;
+
+        return ans;
     }
 }
